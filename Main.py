112b11bbd3eb8cf6e67727f40dffcd196ac5e525
elifah93 <elifalethcantu@gmail.com>
2016-03-01 15:18:03 -0600

    Version working in payroll office: .

--- Main.py
+++ Main.py
@@ -52,7 +52,7 @@ for line in f: # Reads all the lines in the file
 for p in listOfPersons:
     k = 0
     for k in range(0,4):
-        Y = k*62
+        Y = k*64
         name = ""
         suff = ""
         lastName = ""
@@ -60,13 +60,13 @@ for p in listOfPersons:
         emploAddress = ""
         for key in p.keys():
             if(key == "federal_ein"):
-                latexF.write("\n\PlaceText{"+str(95)+"mm}{"+str(53+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(96)+"mm}{"+str(48+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "employer_name"):
-                latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(30+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(23+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "employer_address"):
                 emploAddress = p[key]
             elif(key == "ssn"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(53+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(48+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "emp_name"):
                 name = p[key]
             elif(key == "last_name"):
@@ -76,33 +76,33 @@ for p in listOfPersons:
             elif(key == "employee_address"):
                 address = p[key]
             elif(key == "wages_tips_compensation"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(15+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(8+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "fit_withheld"):
-                latexF.write("\n\PlaceText{"+str(166)+"mm}{"+str(15+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(171)+"mm}{"+str(8+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "ss_wages"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(22+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(15+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "ss_withheld"):
-                latexF.write("\n\PlaceText{"+str(166)+"mm}{"+str(22+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(171)+"mm}{"+str(15+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "med_wages"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(28+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(21+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "med_withheld"):
-                latexF.write("\n\PlaceText{"+str(166)+"mm}{"+str(28+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(171)+"mm}{"+str(21+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "ss_tips"):
-                latexF.write("\n\PlaceText{"+str(95)+"mm}{"+str(22+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(96)+"mm}{"+str(15+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "allocated_tips"):
-                latexF.write("\n\PlaceText{"+str(95)+"mm}{"+str(28+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(96)+"mm}{"+str(21+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "eic_payment"):
                 print(p[key])
             elif(key == "dependent_care"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(34+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(28+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "non_qual_plan"):
-                latexF.write("\n\PlaceText{"+str(166)+"mm}{"+str(34+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(171)+"mm}{"+str(28+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "stat_employee"):
-                latexF.write("\n\PlaceText{"+str(131)+"mm}{"+str(46+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(136)+"mm}{"+str(42+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "retirement_plan"):
-                latexF.write("\n\PlaceText{"+str(143)+"mm}{"+str(46+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(145)+"mm}{"+str(42+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "sick_pay"):
-                latexF.write("\n\PlaceText{"+str(154)+"mm}{"+str(46+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(156)+"mm}{"+str(42+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box14_codea"):
                 print(p[key])
             elif(key == "boxe14_meaninga"):
@@ -116,21 +116,21 @@ for p in listOfPersons:
             elif(key == "boxe14_meaningc"):
                 print(p[key])
             elif(key == "box12_codea"):
-                latexF.write("\n\PlaceText{"+str(95)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(96)+"mm}{"+str(33+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_meaninga"):
-                latexF.write("\n\PlaceText{"+str(105)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(105)+"mm}{"+str(34+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_codeb"):
-                latexF.write("\n\PlaceText{"+str(130)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(135)+"mm}{"+str(33+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_meaningb"):
-                latexF.write("\n\PlaceText{"+str(140)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(145)+"mm}{"+str(34+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_codec"):
-                latexF.write("\n\PlaceText{"+str(166)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(171)+"mm}{"+str(33+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_meaningc"):
-                latexF.write("\n\PlaceText{"+str(176)+"mm}{"+str(40+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(186)+"mm}{"+str(34+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_coded"):
-                latexF.write("\n\PlaceText{"+str(95)+"mm}{"+str(46+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(96)+"mm}{"+str(41+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "box12_meaningd"):
-                latexF.write("\n\PlaceText{"+str(105)+"mm}{"+str(46+Y+(k*1.5))+"mm}{"+p[key]+"}")
+                latexF.write("\n\PlaceText{"+str(105)+"mm}{"+str(42+Y+(k*2))+"mm}{"+p[key]+"}")
             elif(key == "state1_code"):
                 print(p[key])
             elif(key == "state1_ein"):
@@ -168,7 +168,7 @@ for p in listOfPersons:
             #elif(key == "print_instruction"):
              #   print(info[key])
 
-        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(48+Y+(k*1.5))+"mm}{"+name+" "+suff+" "+lastName+"}")
+        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(41+Y+(k*2))+"mm}{"+name+" "+suff+" "+lastName+"}")
 
         i = 0
         for a in address.split('\n'):
@@ -177,8 +177,8 @@ for p in listOfPersons:
             else:
                 a2 = a
             i += 1
-        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(53+Y+(k*1.5))+"mm}{"+a1+"}")
-        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(56+Y+(k*1.5))+"mm}{"+a2+"}")
+        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(46+Y+(k*2))+"mm}{"+a1+"}")
+        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(49+Y+(k*2))+"mm}{"+a2+"}")
 
         i = 0
         for a in emploAddress.split('\n'):
@@ -187,23 +187,23 @@ for p in listOfPersons:
             else:
                 ae2 = a
             i += 1
-        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(34+Y+(k*1.5))+"mm}{"+ae1+"}")
-        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(38+Y+(k*1.5))+"mm}{"+ae2+"}")
+        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(27+Y+(k*2))+"mm}{"+ae1+"}")
+        latexF.write("\n\PlaceText{"+str(15)+"mm}{"+str(31+Y+(k*2))+"mm}{"+ae2+"}")
 
         k += 1
 
-    latexF.write("\n\\begin{textblock*}{5cm}(10mm,260mm)")
+    latexF.write("\n\\begin{textblock*}{5cm}(10mm,265mm)")
     latexF.write("\n\includegraphics[scale=0.15]{UTRGV-Fullname-Black.eps}")
     latexF.write("\n\\end{textblock*}")
 
-    latexF.write("\n\PlaceText{"+str(17)+"mm}{"+str(285)+"mm}{"+ae1+"}")
-    latexF.write("\n\PlaceText{"+str(17)+"mm}{"+str(289)+"mm}{"+ae2+"}")
+    latexF.write("\n\PlaceText{"+str(17)+"mm}{"+str(290)+"mm}{"+ae1+"}")
+    latexF.write("\n\PlaceText{"+str(17)+"mm}{"+str(294)+"mm}{"+ae2+"}")
 
     latexF.write("\n\PlaceText{"+str(80)+"mm}{"+str(327)+"mm}{"+name+" "+suff+" "+lastName+"}")
     latexF.write("\n\PlaceText{"+str(80)+"mm}{"+str(332)+"mm}{"+a1+"}")
     latexF.write("\n\PlaceText{"+str(80)+"mm}{"+str(335)+"mm}{"+a2+"}")
 
-    latexF.write("\n\\begin{textblock*}{5cm}(170mm,265mm)")
+    latexF.write("\n\\begin{textblock*}{5cm}(170mm,270mm)")
     latexF.write("\n\includegraphics[scale=0.25]{FIRST_CLASS_INDICIA.eps}")
     latexF.write("\n\\end{textblock*}")
 
